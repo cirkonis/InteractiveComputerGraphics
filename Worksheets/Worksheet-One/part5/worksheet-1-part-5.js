086dbfe0e3ce2ae7475a8d7b753c824f2b44567a
michaelmarigliano <michael@sharkcell.dk>
2020-09-09 12:26:16 +0200

    translating circle complete - not stoked about the implementation but it works well enough for me to move on.

--- Worksheets/Worksheet-One/part5/worksheet-1-part-5.js
+++ Worksheets/Worksheet-One/part5/worksheet-1-part-5.js
@@ -7,15 +7,52 @@ function init() {
         return;
     }
 
-
-    var white = vec3(1.0, 1.0, 1.0);
-
     program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
-    var triangles =
-        [vec2(-0.5, 0.5), vec2(.5, .5), vec2(0.5,-0.5), vec2(-0.5, -0.5), vec2(-0.5, 0.5), vec2(0.5, -0.5)];
+    var thetaLocation = gl.getUniformLocation(program, "theta");
+    var theta = 0.0;
+    var translateLocation = gl.getUniformLocation(program, "translateY");
+    var translateY =  0.0;
+    var translationSpeed = .04;
+    var numSegments = 30;
+    var radius = .2;
+    var direction = 1;
+    // initially I tried using uniform2fv and having a vec2 translate in the vertex shader so I could use the
+    // indepentant .x and .y values but it wouldn't work for, no shapre would appear at any rate.
+    // I could add the other components individually
+    // then add them combine them here to make a translation vector but it seems like a work around???
+    // settled for just using the one value for now since thats all the behaviour the work sheet called for.
 
+    function uppyDowny(){
+        if (translateY > 1 - radius) {
+            direction = -1;
+        }
+        if (translateY < -1 + radius) {
+            direction = 1;
+        }
+    }
+
+
+    function tick() {
+        theta += 0.0;
+        gl.uniform1f(thetaLocation, theta);
+        uppyDowny();
+        translateY += (translationSpeed * direction);
+        console.log(translateY);
+        console.log(direction);
+        gl.uniform1f(translateLocation, translateY);
+        render(gl, numPoints);
+        requestAnimationFrame(tick);
+    }
+    var circleCenter = vec2(0.0, 0.0);
+    var triangles = [circleCenter];
+    //circle making time
+    for (let i = 0; i < numSegments + 2; i++){
+        theta = (2 * Math.PI * i)/numSegments;
+        var nextVertex = vec2(radius * Math.cos(theta), radius * Math.sin(theta));
+        triangles.push(nextVertex);
+    }
     numPoints = triangles.length;
 
     var vBuffer = gl.createBuffer();
@@ -26,9 +63,12 @@ function init() {
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
 
+    var white = vec3(1.0, 1.0, 1.0);
+    var colors= [];
+    for(let i = 0; i < numPoints; i++){
+        colors.push(white);
+    }
 
-    var colors=
-        [white,  white, white, white, white, white, white];
 
     var cBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
@@ -41,7 +81,6 @@ function init() {
 
 
 
-
     render();
 
 
@@ -50,13 +89,7 @@ function init() {
         gl.viewport(0, 0, canvas.width, canvas.height);
         gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
         gl.clear(gl.COLOR_BUFFER_BIT);
-        gl.drawArrays(gl.TRIANGLES, 0 , numPoints);
-
-    }
-
-
-
-
-
+        gl.drawArrays(gl.TRIANGLE_FAN, 0 , numPoints);}
+    tick();
 }
 window.onload = init;
