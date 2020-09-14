21a0a79575af34f9fb7e21e13efce027c39fb440
michaelmarigliano <michael@sharkcell.dk>
2020-09-14 09:28:06 +0200

    choosing colors to on making points funcionality finished.

--- Worksheets/Worksheet-Two/part2/worksheet-2-part-2.js
+++ Worksheets/Worksheet-Two/part2/worksheet-2-part-2.js
@@ -7,87 +7,77 @@ function init() {
         return;
     }
 
-    var colorsEnum = {
-        Cornflower: 0,
-        Red: 1,
-        Yellow: 2,
-        Green: 3,
-        Blue: 4,
-        Magenta: 5,
-        Cyan: 6,
-        Black: 7,
-    };
-    var backGroundColor = colorsEnum.Cornflower;
-    gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
+    var colors = [
+        vec4(.3921, .5843,.9294, 1), // cornflower
+        vec4(1, 0, 0, 1), // red
+        vec4(1, 1, 0, 1), // yellow
+        vec4(0, 1, 0, 1), // green
+        vec4(0, 0, 1, 1), // blue
+        vec4(1, 0, 1, 1), // magenta
+        vec4(0, 1, 1, 1), // cyan
+        vec4(0, 0, 0, 1), // black
+        vec4(1, 1, 1, 1), //white
+    ];
+    var initialBgColor = colors[0];
+    gl.clearColor(initialBgColor[0], initialBgColor[1], initialBgColor[2], initialBgColor[3]);
+    colsArray = [];
+
     program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
     var vBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    var cBuffer = gl.createBuffer();
+    var vColor = gl.getAttribLocation(program, "vColor");
 
     var maxVertices = 1000;
     var index = 0;
     var numPoints = 0;
-    var vPosition = gl.getAttribLocation(program, "vPosition");
-
-    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
+    var selectedColor = document.getElementById("colorMenu");
 
     canvas.addEventListener("click", function (event) {
         var mousePosition = vec2((2 * event.clientX / canvas.width) - 1, (2 * (canvas.height - event.clientY) / canvas.height) - 1);
+        var currentColor = colors[selectedColor.selectedIndex];
+        gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
         gl.bufferSubData(gl.ARRAY_BUFFER, sizeof['vec2'] * index, flatten(mousePosition));
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+        gl.bufferSubData(gl.ARRAY_BUFFER, sizeof['vec4'] * index, flatten(currentColor));
         numPoints = Math.max(numPoints, ++index);
         index %= maxVertices;
+
     });
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+    gl.bufferData(gl.ARRAY_BUFFER, maxVertices * sizeof['vec2'], gl.STATIC_DRAW);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer)
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
+    gl.bufferData(gl.ARRAY_BUFFER, maxVertices * sizeof['vec4'], gl.STATIC_DRAW);
 
     var clearMenu = document.getElementById("clearColor");
     var clearButton = document.getElementById("clearButton");
     clearButton.addEventListener("click", function(){
-        backGroundColor = clearMenu.selectedIndex;
+        backGroundColor = colors[clearMenu.selectedIndex];
         index = 0;
         numPoints = 0;
-        switch(backGroundColor) {
-            case colorsEnum.Cornflower:
-                gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
-                break;
-            case colorsEnum.Red:
-                gl.clearColor(1, 0, 0, 1);
-                break;
-            case colorsEnum.Yellow:
-                gl.clearColor(0, 1, 1, 1);
-                break;
-            case colorsEnum.Green:
-                gl.clearColor(0, 1, 0, 1);
-                break;
-            case colorsEnum.Blue:
-                gl.clearColor(0, 0, 1, 1);
-                break;
-            case colorsEnum.Magenta:
-                gl.clearColor(1, 1, 0, 1);
-                break;
-            case colorsEnum.Cyan:
-                gl.clearColor(0, 1, 1, 1);
-                break;
-            case colorsEnum.Black:
-                gl.clearColor(0, 0, 0, 1);
-                break;
-        }
+        colsArray = [];
+        gl.clearColor(backGroundColor[0], backGroundColor[1], backGroundColor[2], backGroundColor[3]);
     });
 
-        gl.bufferData(gl.ARRAY_BUFFER, maxVertices * sizeof['vec2'], gl.STATIC_DRAW);
-
-        render();
-
+    render();
 
     function render()
     {
         gl.viewport(0, 0, canvas.width, canvas.height);
-        // gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
         gl.clear(gl.COLOR_BUFFER_BIT);
         if(numPoints > 0) {
             gl.drawArrays(gl.POINTS, 0, numPoints);
         }
-        window.requestAnimFrame(render, canvas);
+        window.requestAnimFrame(render);
     }
+
 }
 window.onload = init;
